442f0e593430a6f959b023a6b699d9771a533f74
JotaJrJr <jhonnyjuniorbot@gmail.com>
2025-02-08 20:46:04 -0300

    inicio dos testes mas a porcarai da instancia mockada do viewmodel e do repository não ta afim por agora

---
+++ test/ir_agora_page_test.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:guia_hoteis_processo/common/domain/models/desconto_model.dart';
+import 'package:guia_hoteis_processo/common/domain/models/motel_model.dart';
+import 'package:guia_hoteis_processo/common/domain/models/periodo_model.dart';
+import 'package:guia_hoteis_processo/common/domain/models/suite_model.dart';
+import 'package:guia_hoteis_processo/common/domain/repositories/motel_repository.dart';
+import 'package:guia_hoteis_processo/common/features/ir_agora/view/ir_agora_page.dart';
+import 'package:guia_hoteis_processo/common/features/ir_agora/viewmodel/ir_agora_view_model.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockMotelRepository extends Mock implements MotelRepository {}
+
+class MockViewModel extends Mock implements IrAgoraViewModel {}
+
+void main() {
+  late IrAgoraViewModel viewModel;
+  late MockMotelRepository mockMotelRepository;
+
+  setUp(() {
+    mockMotelRepository = MockMotelRepository();
+    viewModel = MockViewModel();
+  });
+
+  group('IrAgoraPage Widget Tests', () {
+    testWidgets('Mostrando circularProgress', (tester) async {
+      when(() => viewModel.isLoading).thenReturn(true);
+      when(() => viewModel.moteis).thenReturn([]);
+      when(() => viewModel.discountedSuites).thenReturn([]);
+
+      await tester.pumpWidget(MaterialApp(
+        home: IrAgoraPage(viewModel: viewModel),
+      ));
+
+      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    });
+
+    testWidgets('mostrar "Sem suíte com desconto"', (WidgetTester tester) async {
+      when(() => viewModel.isLoading).thenReturn(false);
+      when(() => viewModel.moteis).thenReturn([]);
+      when(() => viewModel.discountedSuites).thenReturn([]);
+
+      await tester.pumpWidget(MaterialApp(
+        home: IrAgoraPage(viewModel: viewModel),
+      ));
+
+      expect(find.text("Sem suíte com desconto"), findsOneWidget);
+    });
+
+    testWidgets('mostra os moteis com desconto', (WidgetTester tester) async {
+      final discountedSuites = [
+        DiscountedSuiteItem(
+          motel: Motel(
+            fantasia: "",
+            logo: "",
+            bairro: "",
+            distancia: 0.0,
+            qtdAvaliacoes: 0,
+            qtdFavoritos: 0,
+            media: 0.0,
+            suites: [
+              Suite(
+                nome: 'Suite 1',
+                fotos: [],
+                itens: [],
+                categoriaItens: [],
+                qtd: 0,
+                exibirQtdDisponiveis: true,
+                periodos: [
+                  Periodo(
+                    tempoFormatado: '1h',
+                    valor: 100.0,
+                    temCortesia: true,
+                    tempo: "",
+                    valorTotal: 0,
+                    desconto: Desconto(desconto: 10.0),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          suite: Suite(
+            qtd: 0,
+            exibirQtdDisponiveis: true,
+            nome: 'Suite 1',
+            fotos: [],
+            itens: [],
+            categoriaItens: [],
+            periodos: [
+              Periodo(
+                tempoFormatado: '1h',
+                valor: 100.0,
+                tempo: "",
+                valorTotal: 0.0,
+                temCortesia: true,
+                desconto: Desconto(desconto: 10.0),
+              ),
+            ],
+          ),
+        ),
+      ];
+
+      when(() => viewModel.isLoading).thenReturn(false);
+      when(() => viewModel.moteis).thenReturn([]);
+      when(() => viewModel.discountedSuites).thenReturn(discountedSuites);
+
+      await tester.pumpWidget(MaterialApp(
+        home: IrAgoraPage(viewModel: viewModel),
+      ));
+
+      expect(find.byType(PageView), findsOneWidget);
+      expect(find.text("Suite 1"), findsOneWidget);
+    });
+
+    testWidgets('mostra os moteis no listView ', (WidgetTester tester) async {
+      final moteis = [
+        Motel(
+          fantasia: "",
+          logo: "",
+          bairro: "",
+          distancia: 0.0,
+          qtdAvaliacoes: 0,
+          qtdFavoritos: 0,
+          media: 0.0,
+          suites: [
+            Suite(
+              nome: 'Suite 1',
+              fotos: [],
+              itens: [],
+              categoriaItens: [],
+              qtd: 0,
+              exibirQtdDisponiveis: true,
+              periodos: [
+                Periodo(tempoFormatado: '1h', valor: 100.0, desconto: Desconto(desconto: 10.0), tempo: "", valorTotal: 0.0, temCortesia: true),
+              ],
+            ),
+          ],
+        ),
+      ];
+
+      when(() => viewModel.isLoading).thenReturn(false);
+      when(() => viewModel.moteis).thenReturn(moteis);
+      when(() => viewModel.discountedSuites).thenReturn([]);
+
+      await tester.pumpWidget(MaterialApp(
+        home: IrAgoraPage(viewModel: viewModel),
+      ));
+
+      expect(find.byType(ListView), findsOneWidget);
+      expect(find.text("Motel 1"), findsOneWidget);
+    });
+  });
+}
---
+++ test/ir_agora_view_model_test.dart
@@ -0,0 +1,86 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:guia_hoteis_processo/common/domain/models/desconto_model.dart';
+import 'package:guia_hoteis_processo/common/domain/models/motel_model.dart';
+import 'package:guia_hoteis_processo/common/domain/models/periodo_model.dart';
+import 'package:guia_hoteis_processo/common/domain/models/suite_model.dart';
+import 'package:guia_hoteis_processo/common/domain/repositories/motel_repository.dart';
+import 'package:guia_hoteis_processo/common/features/ir_agora/viewmodel/ir_agora_view_model.dart';
+import 'package:mocktail/mocktail.dart';
+
+void main() {
+  late IrAgoraViewModel viewModel;
+  var repository = MockMotelRepository();
+
+  setUp(() {
+    viewModel = IrAgoraViewModel(repository);
+  });
+
+  group('IrAgoraViewModel Tests', () {
+    test('init stat e', () {
+      expect(viewModel.moteis, isEmpty);
+      expect(viewModel.discountedSuites, isEmpty);
+      expect(viewModel.isLoading, isFalse);
+      expect(viewModel.currentPage, equals(0.0));
+    });
+
+    test('updateCurrentPage atualiza a página', () {
+      viewModel.updateCurrentPage(2.0);
+      expect(viewModel.currentPage, equals(2.0));
+    });
+
+    test('getMoteis pega os moteis e filtra pelo filtro lá', () async {
+      // Arrange
+      final motels = [
+        Motel(
+          fantasia: "",
+          logo: "",
+          bairro: "",
+          distancia: 0.0,
+          qtdFavoritos: 0,
+          qtdAvaliacoes: 0,
+          media: 0.0,
+          suites: [
+            Suite(
+              nome: 'Suite 1',
+              fotos: [],
+              itens: [],
+              categoriaItens: [],
+              qtd: 0,
+              exibirQtdDisponiveis: true,
+              periodos: [
+                Periodo(
+                  tempo: "",
+                  valorTotal: 10,
+                  temCortesia: true,
+                  tempoFormatado: '1h',
+                  valor: 100.0,
+                  desconto: Desconto(desconto: 10.0),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ];
+
+      when(repository.getMoteis()).thenAnswer((_) async => motels);
+
+      await viewModel.getMoteis();
+
+      expect(viewModel.moteis, equals(motels));
+      expect(viewModel.discountedSuites.length, equals(1));
+      expect(viewModel.isLoading, isFalse);
+    });
+
+    test('getMoteis dá erro', () async {
+      when(repository.getMoteis()).thenThrow(Exception('Deu ruim ein galera'));
+
+      await viewModel.getMoteis();
+
+      expect(viewModel.moteis, isEmpty);
+      expect(viewModel.discountedSuites, isEmpty);
+      expect(viewModel.isLoading, isFalse);
+    });
+  });
+}
+
+class MockMotelRepository extends Mock implements MotelRepository {}
